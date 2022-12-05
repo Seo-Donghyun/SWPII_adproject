0a24d7c7b9ac1d73dcf1cdbcfdee3c8ac31c653e
Seo-Donghyun <peterpan1103@kookmin.ac.kr>
2022-12-05 21:57:01 +0900

    modified: number range

--- Check.py
+++ Check.py
@@ -4,7 +4,7 @@
 
 class check:
     def numCheck(self, num): # 입력된 숫자가 올바른 숫자인지 확인하는 함수
-        if int(num) <= 0 or int(num) > 26 or num[0] == "0": # 숫자가 1 ~ 25가 아니라면, '4'가 아니라 '04' 같이 입력된다면
+        if int(num) <= 0 or int(num) >= 26 or num[0] == "0": # 숫자가 1 ~ 25가 아니라면, '4'가 아니라 '04' 같이 입력된다면
             return False
         else:
             return True
